01c225a901d27217d27396fab8ad9ea9cf49ebc7
TaLayJai <jeerapatsea@gmail.com>
2025-02-03 06:58:06 +0000

    lab3

---
+++ Doors/Doorknob.java
@@ -0,0 +1,19 @@
+public class Doorknob{
+    private String doorknobID;
+
+    public Doorknob(String doorknobID){
+        this.doorknobID = doorknobID;
+    }
+    
+    public boolean canUnlock(Key key){
+        if (this.doorknobID.equals(key.getKeyID())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public String getDoorknobID(){
+        return this.doorknobID;
+    }
+}
---
+++ Doors/Key.java
@@ -0,0 +1,11 @@
+public class Key{
+    private String keyID;
+
+    public Key(String newKeyID){
+        keyID = newKeyID;
+    }
+    
+    public String getKeyID(){
+        return keyID;
+    }
+}
